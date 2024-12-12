76152a41f1a87b27e546ba2d0119b0a44462b532
musoye <oyebamijimustapha44@gmail.com>
2024-12-12 12:43:50 +0100

    feat: the web app

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify, render_template
+from load_model import predict
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET'])
+def get_predict():
+    return render_template('land.html')
+
+@app.route('/predict', methods=['POST'])
+def post_predict():
+    if request.method == 'POST':
+        dat = request.form
+        payment_type = dat['payment_type']
+        oldbalance = dat['obalance']
+        newbalance = dat['nbalance']
+        withdraw = dat['withdraw']
+        ans = predict(payment_type, oldbalance, newbalance, withdraw)
+        return jsonify({'isFraud': ans})
+    return render_template('land.html')
+
+@app.route('/api/predict', methods=['POST'])
+def post_api_predict():
+    if request.method == 'POST':
+        dat = request.get_json()
+        payment_type = dat['payment_type']
+        oldbalance = dat['obalance']
+        newbalance = dat['nbalance']
+        withdraw = dat['withdraw']
+        ans = predict(payment_type, oldbalance, newbalance, withdraw)
+    return jsonify({'isFraud': ans})
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ load_model.py
@@ -0,0 +1,6 @@
+def predict(payment_type, obalance, nbalance, withdraw):
+
+    # Load the model
+    #predict the outcome
+
+    return True
